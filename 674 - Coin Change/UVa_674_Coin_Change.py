800b483bf4f4a24459daff8588ad2a064042d002
chien-wei <chien-wei@outlook.com>
2018-09-22 11:14:49 -0600

    dp

--- 674 - Coin Change/UVa_674_Coin_Change.py
+++ 674 - Coin Change/UVa_674_Coin_Change.py
@@ -2,3 +2,16 @@
 # Problem description: https://uva.onlinejudge.org/external/6/674.pdf
 # Author: Chien-Wei Sun
 
+import sys
+
+ways_to_change = [0 for _ in range(7490)]
+ways_to_change[0] = 1
+coin_values = [1, 5, 10, 25, 50]
+for v in coin_values:
+    for i in range(v, len(ways_to_change)):
+        ways_to_change[i] += ways_to_change[i-v]
+
+input = sys.stdin.readline().strip()
+while input != '':
+    print(ways_to_change[int(input)])
+    input = sys.stdin.readline().strip()
